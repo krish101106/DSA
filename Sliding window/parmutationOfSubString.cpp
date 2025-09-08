a197afad1ce6c13051e00c84f9190c96f793a443
krish101106 <krishgajjar051@gmail.com>
2025-09-08 08:01:24 +0530

    8sep

--- Sliding window/parmutationOfSubString.cpp
+++ Sliding window/parmutationOfSubString.cpp
@@ -6,46 +6,44 @@ using namespace std;
 
 class Solution {
 public:
-    bool isMatch(int arr1[], int arr2[]) {
-        for (int i = 0; i < 26; i++) {
-            if (arr1[i] != arr2[i]) {
+    bool isMatch(int h1[], int h2[]) {
+        for(int i = 0; i < 26; i++) {
+            if (h1[i] != h2[i]) {
                 return false;
             }
         }
         return true;
     }
 
-    bool search(string& txt, string& pat) {
-        // Step 1: Create frequency map for the pattern (Corrected loop)
-        int patMap[26] = {0};
-        for (int i = 0; i < pat.length(); i++) {
-            patMap[pat[i] - 'a']++;
+    bool checkInclusion(string s1, string s2) {
+        if (s1.length() > s2.length()) {
+            return false;
         }
 
-        // Create frequency map for the FIRST window
-        int windowMap[26] = {0};
-        for (int i = 0; i < pat.length() && i < txt.length(); i++) {
-            windowMap[txt[i] - 'a']++;
+        int h1[26] = {0};
+        int h2[26] = {0};
+
+        for (int i = 0; i < s1.length(); i++) {
+            h1[s1[i] - 'a']++;
+            h2[s2[i] - 'a']++;
         }
 
-        // Check the first window
-        if (isMatch(patMap, windowMap)) {
+        if (isMatch(h1, h2)) {
             return true;
         }
 
-        // Step 2: Slide the window efficiently across the rest of the text
-        // NO inner loop needed here!
-        for (int i = pat.length(); i < txt.length(); i++) {
-            // Add the new character entering the window from the right
-            windowMap[txt[i] - 'a']++;
+        int s = 0;
+        int e = s1.length();
 
-            // Remove the old character leaving the window from the left
-            windowMap[txt[i - pat.length()] - 'a']--;
-
-            // Check if the current window is a match
-            if (isMatch(patMap, windowMap)) {
+        while (e < s2.length()) {
+            h2[s2[s] - 'a']--;
+            h2[s2[e] - 'a']++;
+            
+            if (isMatch(h1, h2)) {
                 return true;
             }
+            s++;
+            e++;
         }
 
         return false;
