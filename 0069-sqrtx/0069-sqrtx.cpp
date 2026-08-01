343e1bd238e5df63fbab4afe6aed0f54d53de238
Krushan Mesvaniya <krishgajjar051@gmail.com>
2026-08-01 18:10:25 +0530

    Time: 0 ms (100%), Space: 8.7 MB (15.06%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        
+        int s{0};
+        int e=x;
+        long long int mid{-1};
+        int ans{-1};
+
+
+        while(s<=e){
+            mid=s+((e-s)/2);
+            if((mid*mid)<=x){
+                ans=mid;
+                s=mid+1;
+            }else{
+                e=mid-1;
+            }
+        }
+
+        return ans;
+        
+    }
+};
